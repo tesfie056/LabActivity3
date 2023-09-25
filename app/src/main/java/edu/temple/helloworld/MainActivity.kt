b7bde09d535b490926f72c9928b0b1abd866a37a
tesfie056 <143583774+tesfie056@users.noreply.github.com>
2023-09-25 16:15:37 -0400

    created a fork and cloned it. created new brunch through Git Created a new brunch locally via the android studio. added constraints to the layout view, button and text inpute.
    created variable for the text input
    created function that shows error if text input is blank.

--- app/src/main/java/edu/temple/helloworld/MainActivity.kt
+++ app/src/main/java/edu/temple/helloworld/MainActivity.kt
@@ -5,24 +5,40 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
 
     // Declare view properties - the first one is done for you
     lateinit var displayTextView: TextView
-
+    lateinit var nameEditText: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         // Initialize with views defined in Layout - the first one is done for you
         displayTextView = findViewById(R.id.displayTextView)
-
+        nameEditText = findViewById(R.id.nameEditText)
         
         findViewById<Button>(R.id.clickMeButton).setOnClickListener {
-            displayTextView.text = "Hello, ${findViewById<EditText>(R.id.nameEditText).text}"
+            var hasError = false
+            nameEditText.run {
+                if (text.isBlank() ){
+                    hasError = true
+                    showError( this,"Can not be blank")
+                } else {
+                    displayTextView.text = "Hello, ${findViewById<EditText>(R.id.nameEditText).text}"
+                }
+            }
+
+
+
         }
 
 
+    }
+
+    private fun showError(view: TextView, message: CharSequence) {
+view.error = message
     }
 }
